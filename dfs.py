82bd50aa2af17e03f9dccc9d0d14cdad2f405356
Jullliya <vamoyu2@gmail.com>
2024-05-06 14:17:26 +0300

    feature add(dfs.py) find path length

--- dfs.py
+++ dfs.py
@@ -9,21 +9,22 @@ g = []
 for i in range(n):
     g.append([0]*n)
 
+path = 0
 start = int(input("Enter the start point: ")) - 1
+end = int(input("Enter the last point: ")) - 1
 visited = [False] * (n + 1)
 prev = [-1] * (n + 1)
 
 for i in range(m):
     g[pair[i][0]-1][pair[i][1]-1] = 1
 
-def dfs(start, visited, prev, g):
+def dfs(start, end, visited, prev, g, path):
     visited[start] = True
     for u in range(len(g[start])):
-        if not visited[u] and g[start][u]:
+        if not visited[u] and g[start][u] and u != end:
+            path = path + 1
             prev[u] = start
-            dfs(u, visited, prev, g)
+            dfs(u, end, visited, prev, g, path)
+    return path
 
-dfs(start, visited, prev, g)
-for i in range(len(prev)):
-    if prev[i] != -1:
-        print(prev[i]+1, "->", i + 1)
+print(dfs(start, end, visited, prev, g, path))
