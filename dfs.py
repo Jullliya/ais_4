5bcb7cdf6f401a0cb209f09da567d72c68d7bc60
Jullliya <vamoyu2@gmail.com>
2024-05-06 15:03:50 +0300

    refactor(dfs.py) add comments

--- dfs.py
+++ dfs.py
@@ -1,7 +1,11 @@
+# введем значение размерности графа
 n = int(input("Enter the count of points in graph: "))
 pair = []
+# введем количество пар соединенных вершин
 m = int(input("Enter the number of pairs: "))
+# введем пары соединений вершин
 print("Enter the pair of point: ")
+
 for i in range(m):
     pair.append([int(x) for x in input().split()])
 
@@ -9,6 +13,7 @@ g = []
 for i in range(n):
     g.append([0]*n)
 
+# ввод необходимых значений
 path, start, end = 0, 0, 0
 while start > n-1 or start <= 0:
     start = int(input("Enter the start point: ")) - 1
@@ -22,13 +27,14 @@ while end > n-1 or end <= 0:
         print("Incorrect value of start number, end more than graph dimensions.")
     if end < 0:
         print("Incorrect value of start number, end cannot be negative or zero. ")
-
 visited = [False] * (n + 1)
 prev = [-1] * (n + 1)
 
+# преобразователь парных значений координат в матрицу
 for i in range(m):
     g[pair[i][0]-1][pair[i][1]-1] = 1
 
+# функция обхода графа в глубину с подсчетом длины маршрута
 def dfs(start, end, visited, prev, g, path):
     visited[start] = True
     for u in range(len(g[start])):
@@ -38,4 +44,5 @@ def dfs(start, end, visited, prev, g, path):
             dfs(u, end, visited, prev, g, path)
     return path
 
+# вызов функции
 print(dfs(start, end, visited, prev, g, path))
